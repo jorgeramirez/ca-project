ec5d90e4c6973b33d35c0b816b7609196b19d6d7
Jorge Ramírez <jorgeramirez1990@gmail.com>
2019-11-06 00:42:30 +0100

    Update script
    Signed-off-by: Jorge Ramírez <jorgeramirez1990@gmail.com>

--- train-sfid.sh
+++ train-sfid.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cd SF-ID-Network-For-NLU/
-python train.py --dataset=atis --priority_order=slot_first --use_crf=True
+python train.py --dataset=atis --priority_order=slot_first --use_crf=True > /tmp/train_sfid.log 2>&1 &
