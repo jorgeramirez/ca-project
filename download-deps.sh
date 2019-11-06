59c949ade52eb39941dbb5c6a8667ad9ee98fc94
Jorge Ramírez <jorgeramirez1990@gmail.com>
2019-11-06 09:54:59 +0100

    Use glove
    Signed-off-by: Jorge Ramírez <jorgeramirez1990@gmail.com>

---
+++ download-deps.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Glove embeddings
+wget http://nlp.stanford.edu/data/glove.6B.zip
+unzip glove.6B.zip -d glove.6B
--- train-sfid.sh
+++ train-sfid.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 cd SF-ID-Network-For-NLU/
+
 python train.py --dataset=atis --priority_order=slot_first --use_crf=True > /tmp/train_sfid.log 2>&1 &
+
+
+# using glove embeddings
+python train.py --dataset=atis --priority_order=slot_first --use_crf=True --embedding_path=../glove.6B/glove.6B.300d.txt > /tmp/train_sfid.log 2>&1 &
